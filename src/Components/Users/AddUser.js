bc44c0f2fd5f2679e5251c6e65c7edc6e33630e7
Shannkar022 <shankarweb22@gmail.com>
2021-10-14 21:57:41 +0530

    Adding Wrapper rather than <div>

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -4,6 +4,7 @@ import styles from './AddUser.module.css'
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
     const [enteredUserName, setEnteredUserName] = useState('');
@@ -42,7 +43,7 @@ const AddUser = (props) => {
         setError(null);
     }
     return (
-        <>
+        <Wrapper>
             {error  && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>},
             <Card className={styles.input}>
                 <form onSubmit={addUserHandler}>
@@ -53,7 +54,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </>
+        </Wrapper>
     );
 }
 
